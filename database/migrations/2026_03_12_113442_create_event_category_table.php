e4f634866183cfdd6926a6e12fe4611002abd4a0
Miracle Steven <miraclesteven05@gmail.com>
2026-03-14 06:06:13 +0700

    fix(branch): restore mistakenly deleted migrations and composer.lock

---
+++ database/migrations/2026_03_12_113442_create_event_category_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('event_category', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 45);
+            $table->string('description', 100)->nullable();
+            $table->timestamp('created_at')->nullable()->useCurrent();
+            $table->timestamp('updated_at')->useCurrentOnUpdate()->nullable()->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('event_category');
+    }
+};
---
+++ database/migrations/2026_03_12_113442_create_wishlists_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wishlists', function (Blueprint $table) {
+            $table->char('user_id', 36);
+            $table->char('event_id', 36)->index('fk_wishlist_events_idx');
+
+            $table->primary(['user_id', 'event_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wishlists');
+    }
+};
