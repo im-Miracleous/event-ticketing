c5381bd0abfdbbfe1844dc5d3c4325611d4c471a
Febrian123-create <2472039@maranatha.ac.id>
2026-03-23 22:43:17 +0700

    Checkout & Booking

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/', [App\Http\Controllers\EventCatalogController::class, 'index'])->name('home');
     Route::get('/events', [App\Http\Controllers\EventCatalogController::class, 'index'])->name('events.index');
 
+    // Checkout & Booking
+    Route::get('/events/{event}/checkout', [App\Http\Controllers\CheckoutController::class, 'show'])->name('checkout.show');
+    Route::post('/checkout', [App\Http\Controllers\CheckoutController::class, 'store'])->name('checkout.store');
+    Route::get('/checkout/{transactionId}/payment', [App\Http\Controllers\CheckoutController::class, 'payment'])->name('checkout.payment');
+    Route::post('/checkout/{transactionId}/confirm', [App\Http\Controllers\CheckoutController::class, 'confirmPayment'])->name('checkout.confirm');
+    Route::post('/checkout/{transactionId}/cancel', [App\Http\Controllers\CheckoutController::class, 'cancel'])->name('checkout.cancel');
+    Route::get('/checkout/{transactionId}/result', [App\Http\Controllers\CheckoutController::class, 'result'])->name('checkout.result');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__.'/auth.php';
